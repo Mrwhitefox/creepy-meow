6b6d5f523cad166fe79fd17a35e55e445fcb36ea
L.C <Mrwhitefox@users.noreply.github.com>
2015-10-30 17:35:26 +0100

    Update testcurl.sh

--- testcurl.sh
+++ testcurl.sh
@@ -1,6 +1,2 @@
-#curl -H "Content-Type application/json" \
-#    -X POST -d '{"id":"1","vlan":"8","ip":"192.1968.1.1/24"}'\
-#    http://datamancer/api/container/create 
-
-curl --data "id=2&vlan=2&ip=255.168.1.252/21" \
-    http://datamancer/api/container/create 
+curl --data "id=5&vlan=2&ip=192.168.2.5/24" \
+    http://localhost/api/container/create 
